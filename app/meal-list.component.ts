bf874c9f756987b46fd77405657a9f2a1e854c62
Nicole Sanders <nsanders9022@gmail.com>
2017-03-31 11:30:10 -0700

    add faulty function to display the total number of calories eaten

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -4,12 +4,20 @@ import { Meal } from './meal.model';
 @Component({
   selector: 'meal-list',
   template: `
-    <h2>Sort by Calories</h2>
-    <select (change)="onChange($event.target.value)">
-      <option value="allMeals">All Meals</option>
-      <option value="less500">Meals under 500 calories</option>
-      <option value="more500">Meals 500 calories or more</option>
-    </select>
+    <div class="row">
+      <div class="col-sm-6">
+        <h2>Sort by Calories</h2>
+        <select (change)="onChange($event.target.value)">
+          <option value="allMeals">All Meals</option>
+          <option value="less500">Meals under 500 calories</option>
+          <option value="more500">Meals 500 calories or more</option>
+        </select>
+      </div>
+      <div class="col-sm-6">
+        <h2>Total Calories</h2>
+        <h3>{{totalCalories()}}</h3>
+      </div>
+    </div>
     <hr>
       <h2>Meals</h2>
       <div *ngFor="let currentMeal of childMealList | calorieCount:filterByCalories">
@@ -24,7 +32,8 @@ import { Meal } from './meal.model';
 export class MealListComponent {
   @Input() childMealList: Meal[];
   @Output() clickSender = new EventEmitter();
-  filterByCalories: string = "calorieCount"
+  filterByCalories: string = "calorieCount";
+  calorieTotal: number = 0;
 
   editButtonClicked(mealToEdit: Meal) {
     this.clickSender.emit(mealToEdit);
@@ -32,5 +41,12 @@ export class MealListComponent {
 
   onChange(optionFromMenu) {
     this.filterByCalories = optionFromMenu;
+  }
+
+  totalCalories() {
+    for (var meal of this.childMealList) {
+      this.calorieTotal += meal.calories;
+    }
+    return this.calorieTotal;
   }
 }
